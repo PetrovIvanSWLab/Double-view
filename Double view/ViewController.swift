b96387b3f9ceccf9048f4914ae5847e1b7e6903d
Petrov Ivan <pia888@list.ru>
2021-10-21 21:29:19 +0700

    Next commit

---
+++ Double view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Double view
+//
+//  Created by Иван Иванов on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
